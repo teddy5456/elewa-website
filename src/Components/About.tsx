ce8fd41e679e6d1ae6c1c56fc3a0b912e7d31629
teddy <teddywambua5456@gmail.com>
2023-11-03 17:53:36 +0300

    :art:Culture section added

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -153,7 +153,44 @@ function About() {
             <button onClick={handleMoveLeft}>Previous</button>
             <button onClick={handleMoveRight}>Next</button>
           </div>
-          
+        </div>
+      </div>
+
+
+      <div className='culture-section'>
+        <div className='culture-section-inner'>
+            <div className='culture-section-title'>
+                <h1>Our Cultures & Values</h1>
+                <p>Our culture, <span className='italic-codeT'>CODE-(T)</span>, defines us. It protects and guides us to success. It keeps our team and management accountable to itself, its objective, and our long-term mission.</p>
+            </div>
+            <div className='culture-section-code '>
+                <div className='code-box left'>
+                    <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png'></img>
+                    <h2>Cooperative</h2>
+                    <p>One for all ,all for one! We have a cooperative and open mindset. If one of us grows, all will follow.</p>
+                </div>
+                <div className='code-box right'>
+                    <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/ownership_yno4a2.png'></img>
+                    <h2>Ownership</h2>
+                    <p>We are dependable and honour our commitments. When things don't go as planned, as they sometimes do, we communicate early to avoid catastrophe.</p>
+                </div>
+                <div className='code-box left'>
+                    <img src="https://static.thenounproject.com/png/1721983-200.png"></img>
+                    <h2>Detail</h2>
+                    <p>We are a "Centre of Excellence" as per global standards. We only compete with ourselves, but find in the global standard our only worthy rival.</p>
+                </div>
+                <div className='code-box right'>
+                    <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png'></img>
+                    <h2>Empathy</h2>
+                    <p>We are nothing without people, and people have different contexts and needs. Our organization acts as a safe haven and shield for those in need.</p>
+                </div>
+                <div className='code-box bottom'>
+                    <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690300/elewa-group-website/Icons/PNG/Transparancy_fqal8q.png'></img>
+                    <h2>Transparency</h2>
+                    <p>Transparency, an open mind, and an open heart hold our organization together. We commit to and promote full transparency to ourselves, our stakeholders, and our beneficiaries. We furthermore invest heavily in open-source and open-data projects, as true potential lies everywhere. Not just within.</p>
+                </div>
+
+            </div>
         </div>
       </div>
     </div>
